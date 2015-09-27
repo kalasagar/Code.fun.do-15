2d4a7c1101475a440d750b98aead15c25cc58255
sagar <pogirikalasagar@gmail.com>
2015-09-27 05:52:30 +0545

    blankyapp chaty

---
+++ BlankSharedApp3/BlankSharedApp3/BlankSharedApp3.Shared/js/chat.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var ChatApp = window.ChatApp = window.ChatApp || {};
+
+    var Chat = ChatApp.Chat = function (socket) {
+        this.socket = socket;
+    };
+
+    Chat.prototype.sendMessage = function (data) {
+        this.socket.emit("message", { text: data.text });
+    };
+
+    Chat.prototype.processCommand = function (data) {
+        var spaceIndex = data.text.indexOf(" ");
+        if (data.text.substring(1, spaceIndex) === "nick") {
+            this.socket.emit("nicknameChangeRequest", data)
+        } else if (data.text.substring(1, spaceIndex) === "join") {
+            this.socket.emit("roomChangeRequest", {
+                room: data.text.substring(spaceIndex + 1)
+            });
+        }
+    }
+})();
---
+++ BlankSharedApp3/BlankSharedApp3/BlankSharedApp3.Shared/js/chat_us.js
@@ -0,0 +1,59 @@
+﻿(function () {
+    var ChatApp = window.ChatApp = window.ChatApp || {};
+
+    var ChatUI = ChatApp.ChatUI = function (chat, $el) {
+        this.chat = chat;
+        this.$el = $el;
+        this.nicknames = {};
+
+        var ui = this;
+        this.chat.socket.on('message', ui.renderMessage.bind(ui));
+        this.chat.socket.on('nicknameChangeResult', ui.renderMessage.bind(ui))
+        this.$el.find("form.new-message").on("submit", ui.getMessage.bind(ui));
+        this.chat.socket.on('nicknamesUpdate', ui.renderNicknames.bind(ui));
+        this.chat.socket.on('roomList', ui.renderRoomList.bind(ui));
+    }
+
+    ChatUI.prototype.getMessage = function (event) {
+        event.preventDefault();
+        var data = $(event.currentTarget).serializeJSON();
+
+        if (data.text.charAt(0) === "/") {
+            this.chat.processCommand(data);
+        } else {
+            this.chat.sendMessage(data);
+        }
+
+        $(event.currentTarget).find('#new-message-text').val("");
+    }
+
+    ChatUI.prototype.renderMessage = function (data) {
+        var $li = $("<li>");
+        $li.text(data.text);
+        this.$el.find("ul.chat").append($li);
+    }
+
+    ChatUI.prototype.renderNicknames = function (data) {
+        this.$el.find("ul.users").empty();
+        var $li;
+        var that = this;
+
+        Object.keys(data.nicknames).forEach(function (key) {
+            $li = $("<li>").text(data.nicknames[key]);
+            that.$el.find("ul.users").append($li);
+        });
+    }
+
+    ChatUI.prototype.renderRoomList = function (data) {
+        this.$el.find("ul.room-users").empty();
+        var $li;
+        var that = this;
+
+        data.roomUsers.forEach(function (user) {
+            $li = $("<li>").text(user);
+            that.$el.find("ul.room-users").append($li);
+        });
+    }
+
+
+})();
